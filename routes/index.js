5981743d11af4d0fb48ca36c0d529528897e0164
Olga Thompson <olgatj2007@gmail.com>
2021-01-06 15:33:19 -0500

    working on log in form

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('navbar-home', {} );
+  res.send('message');
 });
 
 module.exports = router;
