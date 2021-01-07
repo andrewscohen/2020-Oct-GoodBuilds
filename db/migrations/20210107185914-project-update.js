9309affb78a540506c6d868e49c3091bae865094
Andrew <andrewcohen14@gmail.com>
2021-01-07 16:05:30 -0500

    Edited Requested Change for down: of Project-Update

--- db/migrations/20210107185914-project-update.js
+++ db/migrations/20210107185914-project-update.js
@@ -4,8 +4,23 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.changeColumn('Projects', 'createdAt', { defaultValue: new Date(), type: Sequelize.DATE });
     await queryInterface.changeColumn('Projects', 'updatedAt', { defaultValue: new Date(), type: Sequelize.DATE });
+
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+    */
   },
 
-  down: (queryInterface, Sequelize) => {
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('Projects', 'createdAt', {
+      defaultValue: null,
+      allowNull: true,
+    });
+    await queryInterface.changeColumn('Projects', 'createdAt', {
+      defaultValue: null,
+      allowNull: true,
+    });
   }
 };
