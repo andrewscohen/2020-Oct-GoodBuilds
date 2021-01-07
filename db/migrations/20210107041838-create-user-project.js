f29b56234fde58de617b59786026854616e4595f
Andrew <andrewcohen14@gmail.com>
2021-01-07 00:00:39 -0500

    Added references to Migration Files

--- db/migrations/20210107041838-create-user-project.js
+++ db/migrations/20210107041838-create-user-project.js
@@ -10,11 +10,13 @@ module.exports = {
       },
       userId: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: { model: 'Users' }
       },
       projectId: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: { model: 'Projects' }
       },
       projectDeadline: {
         type: Sequelize.DATEONLY
