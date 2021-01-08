91a95603280ab9f9426811625ae6b03ec9225a70
Amber Horn <amberjolie@Ambers-Mac-mini.local>
2021-01-07 18:14:08 -0600

    thursEOD

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const { sequelize } = require('./db/models');
 const { restoreUser } = require('./auth');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/user');
+const projectRouter = require('./routes/project');
 
 
 //APP SETUP
@@ -44,6 +45,7 @@ store.sync();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', projectRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
