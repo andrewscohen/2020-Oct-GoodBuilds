65de424400cdc1e81b41e2e2db7c65c79d8ffcb3
Michael DiGiovanni <midigi@comcast.net>
2021-01-10 22:52:23 -0500

    few css tweaks

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(sessionSecret));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static('public'));
 
 const store = new SequelizeStore({ db: sequelize });
 
