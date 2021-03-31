373e069f7f87ccb2f45254dd997e5cb4305ea9e2
Olga Thompson <olgatj2007@gmail.com>
2021-03-31 17:49:45 -0400

    moved review post route to projects

--- routes/project.js
+++ routes/project.js
@@ -187,6 +187,52 @@ router.post('/project/delete/:id(\\d+)', csrfProtection,
         res.redirect('/projects');
     }));
 
+const reviewValidators = [
+  check('difficultyLevel')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a level of difficulty to submit'),
+  check('rating')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a level of enjoyment to submit'),
+  check('completionTime')
+      .exists({ checkFalsy: true })
+      .withMessage('How long did it take you to build your beautiful new furniture? (Required to submit)')
+      .isLength({ max: 50 })
+      .withMessage('Completion time must not be more than 50 characters long'),
+];
+
+
+
+// attempting to implement  review validators
+// const review = db.Review.build(
+//   { difficultyLevel, content, rating, completionTime, userId: req.session.auth.userId, projectId }
+// );
+
+// const validatorErrors = validationResult(req);
+
+// if (validatorErrors.isEmpty()) {
+//   await review.save();
+//   res.redirect(`/projects/${projectId}`);
+// } else {
+//   const errors = validatorErrors.array().map((error) => error.msg);
+//   req.locals.errors = errors;
+//   res.redirect(`/projects/${projectId}`);
+//   next(errors);
+// }
+
+ router.post(
+     "/project/reviews",
+     asyncHandler(async (req, res) => {
+       // console.log(req.session.user.id)
+       let { difficultyLevel, content, rating, completionTime, projectId } = req.body;
+       difficultyLevel = parseInt(difficultyLevel);
+       console.log(req.body);
+       rating = parseInt(rating);
+       const review = await db.Review.create({ difficultyLevel, content, rating, completionTime, userId: req.session.auth.userId, projectId });
+       console.log('Saved Review!')
+       res.redirect(`/projects/${projectId}`);
+     })
+   );
 
 
 
