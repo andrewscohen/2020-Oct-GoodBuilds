f1d58e08f5248a9b1f323369c2b875e669e00d6a
Andrew <andrewcohen14@gmail.com>
2021-04-01 20:36:11 -0400

    Attempt at conditional form rendering

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,8 +6,14 @@ window.addEventListener("load", (event)=>{
 //show and hide edit review form
 //in progress
 $(document).ready(function(){
+    let buttonClicked = false
     $("#review-btn").click(function(){
      console.log('clicked the edit button')
+     if (buttonClicked === true) {
+        buttonClicked = false
+    } else {
+        buttonClicked = true;
+    }
      $("#review-form").toggleClass("form-hide")
     });
   });
@@ -18,7 +24,7 @@ $(document).ready(function(){
         console.log('delete button clicked')
         let reviewId = parseInt(this.id.slice(-1))
         $.ajax({
-            url: '/projects/reviews/delete',
+            url: '/project/reviews/delete',
             type: 'DELETE',
             data: {"reviewId": reviewId},
             success: function(result) {
